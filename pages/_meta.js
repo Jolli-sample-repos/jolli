b41241559a069cfabd4854676965b43e6004eeac
Summer Fang <summer.fang@jolli.ai>
2025-12-05 09:57:32 -0800

    Update pages/_meta.js
    Updated via Jolli repository editor

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,8 +1,10 @@
 export default {
-  "index": "Home",
-  "get-start-document": "Get start document",
-  "test-added-article": "Test added article",
-  "introduction": "Introduction",
-  "testtest": "testtest",
-  "test": "test"
+  introduction: "Introduction",
+  "get-start-document": "Get Started",
+
+  testing: { type: "separator", title: "Testing" },
+
+  test: "Test",
+  "test-added-article": "Test Added Article",
+  testtest: "TestTest"
 }
